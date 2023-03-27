7c39eaf2ee42e88b2c3ccf13d0920409a6eaa129
GeorgioArmani <117245302+GeorgioArmani@users.noreply.github.com>
2023-03-27 14:22:46 +0800

    Update test_all_pets_have_different_name.py

--- tests/test_all_pets_have_different_name.py
+++ tests/test_all_pets_have_different_name.py
@@ -7,9 +7,11 @@ from selenium.webdriver.common.by import By
 def test_all_pets_have_different_names(go_to_my_pets):
    '''Поверка того, что на странице "Мои питомцы" у всех питомцев разные имена'''
 
-   # Сохранение элементов с данными о питомцах в переменную "pet_data"
+   # Установка явного ожидания
    element = WebDriverWait(pytest.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
+   
+   # Сохранение элементов с данными о питомцах в переменную "pet_data"
    pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
    # Перебираются данные из переменной "pet_data". Сохраняются имя, возраст и порода, остальное меняется на пустую строку и разделяется по пробелу
