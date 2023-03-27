da770a3529a5549784677d9868253783d4a99cef
GeorgioArmani <117245302+GeorgioArmani@users.noreply.github.com>
2023-03-27 14:03:49 +0800

    Update test_no_duplicate_pet_names.py

--- tests/test_no_duplicate_pet_names.py
+++ tests/test_no_duplicate_pet_names.py
@@ -4,43 +4,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 
-def test_no_duplicate_pets(go_to_my_pets):
-    '''Поверяем что на странице со списком моих питомцев нет повторяющихся питомцев'''
+def test_no_duplicate_pet_names(go_to_my_pets):
+    '''Проверка того, что на странице "Мои питомцы" нет повторяющихся имен питомцев'''
 
-    # Устанавливаем явное ожидание
+    # Установка явного ожидания
     element = WebDriverWait(pytest.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
 
-    # Сохраняем в переменную pet_data элементы с данными о питомцах
-    pet_data = pytest.driver.find_elements_by_css_selector('.table.table-hover tbody tr')
+    # Сохранение элементов с данными о питомцах в переменную "pet_data"
+    pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
-    # Перебираем данные из pet_data, оставляем имя, возраст, и породу остальное меняем на пустую строку
-    # и разделяем по пробелу.
+    # Перебираются данные из переменной "pet_data". Сохраняются имя, возраст и порода, остальное меняется на пустую строку и разделяется по пробелу
     list_data = []
     for i in range(len(pet_data)):
         data_pet = pet_data[i].text.replace('\n', '').replace('×', '')
         split_data_pet = data_pet.split(' ')
         list_data.append(split_data_pet)
 
-    # Склеиваем имя, возраст и породу, получившиеся склееные слова добавляем в строку
-    # и между ними вставляем пробел
+    # Склеиваются имя, возраст и порода. Получившиеся склееные слова добавляются в строку и между ними вставляется пробел
     line = ''
     for i in list_data:
         line += ''.join(i)
         line += ' '
 
-    # Получаем список из строки line
+    # Получение списка из строки "line"
     list_line = line.split(' ')
 
-    # Превращаем список в множество
+    # Превращение списка в множество
     set_list_line = set(list_line)
 
-    # Находим количество элементов списка и множества
+    # Нахождение количества элементов списка и множества
     a = len(list_line)
     b = len(set_list_line)
 
-    # Из количества элементов списка вычитаем количество элементов множества
+    # Из количества элементов списка вычитается количество элементов множества
     result = a - b
 
-    # Если количество элементов == 0 значит карточки с одинаковыми данными отсутствуют
+    # Если количество элементов == 0, то карточки с одинаковыми данными отсутствуют
     assert result == 0
