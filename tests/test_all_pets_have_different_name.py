6c72275a8563a2b031e0502097cdc5d3dfaca2a4
GeorgioArmani <117245302+GeorgioArmani@users.noreply.github.com>
2023-03-27 13:56:12 +0800

    Update test_all_pets_have_different_name.py

--- tests/test_all_pets_have_different_name.py
+++ tests/test_all_pets_have_different_name.py
@@ -5,23 +5,23 @@ from selenium.webdriver.common.by import By
 
 
 def test_all_pets_have_different_names(go_to_my_pets):
-   '''Поверяем что на странице со списком моих питомцев, у всех питомцев разные имена'''
+   '''Поверка того, что на странице "Мои питомцы" у всех питомцев разные имена'''
 
+   # Сохранение элементов с данными о питомцах в переменную "pet_data"
    element = WebDriverWait(pytest.driver, 10).until(
-      EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
-   # Сохраняем в переменную pet_data элементы с данными о питомцах
-   pet_data = pytest.driver.find_elements_by_css_selector('.table.table-hover tbody tr')
+      EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
+   pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
-   # Перебираем данные из pet_data, оставляем имя, возраст, и породу остальное меняем на пустую строку
-   # и разделяем по пробелу.Выбераем имена и добавляем их в список pets_name.
+   # Перебираются данные из переменной "pet_data". Сохраняются имя, возраст и порода, остальное меняется на пустую строку и разделяется по пробелу
+   # Выбираются имена и добавляются в список "pets_name"
    pets_name = []
    for i in range(len(pet_data)):
       data_pet = pet_data[i].text.replace('\n', '').replace('×', '')
       split_data_pet = data_pet.split(' ')
       pets_name.append(split_data_pet[0])
 
-   # Перебираем имена и если имя повторяется то прибавляем к счетчику r единицу.
-   # Проверяем, если r == 0 то повторяющихся имен нет.
+   # Перебираются имена и, если имя повторяется, к счетчику "r" прибавляется единица
+   # Если r == 0, то повторяющихся имен нет
    r = 0
    for i in range(len(pets_name)):
       if pets_name.count(pets_name[i]) > 1:
