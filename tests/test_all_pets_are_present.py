8c167aad643e832477814ce8fb622988156d6dcd
GeorgioArmani <117245302+GeorgioArmani@users.noreply.github.com>
2023-03-27 14:24:22 +0800

    Update test_all_pets_are_present.py

--- tests/test_all_pets_are_present.py
+++ tests/test_all_pets_are_present.py
@@ -7,14 +7,18 @@ from selenium.webdriver.common.by import By
 def test_all_pets_are_present(go_to_my_pets):
    '''Проверка того, что на странице "Мои питомцы" присутствуют все питомцы'''
 
-   # Сохранение элементов статистики в переменную "statistic" 
+   # Установка явного ожидания
    element = WebDriverWait(pytest.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR, '.\\.col-sm-4.left')))
+   
+   # Сохранение элементов статистики в переменную "statistic" 
    statistic = pytest.driver.find_elements(By.CSS_SELECTOR, '.\\.col-sm-4.left')
 
-   # Сохранение элементов карточек питомцев в переменную "pets"
+   # Установка явного ожидания
    element = WebDriverWait(pytest.driver, 10).until(
       EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
+   
+   # Сохранение элементов карточек питомцев в переменную "pets"
    pets = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
    # Получение количества питомцев из данных статистики
